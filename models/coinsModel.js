220b75bc786b3353198ee956bca18f83c6675d4d
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2023-04-26 10:58:49 -0300

    s

--- models/coinsModel.js
+++ models/coinsModel.js
@@ -9,42 +9,42 @@ const coinsSchema = new Schema({
 
 const coinsModel = mongoose.model("coins", coinsSchema);
 
-coinsModel.collection.dropIndex("in_1", function(err, result) {
-    if (err) {
-      console.log("Error eliminando índice:", err);
-    } else {
-      console.log("Índice eliminado:", result);
-    }
-  });
+// coinsModel.collection.dropIndex("in_1", function(err, result) {
+//     if (err) {
+//       console.log("Error eliminando índice:", err);
+//     } else {
+//       console.log("Índice eliminado:", result);
+//     }
+//   });
 
-coinsModel.countDocuments({}, function(err, count) {
-    if (count === 0) {
-        const defaultCoinsModel = [
-            {
-                name: 'bitcoin',
-                interestRate: 0.005,
-                id: 1
-              },
-              {
-                name: 'etherium',
-                interestRate: 0.0042,
-                id: 2
-              },
-              {
-                name: 'cardano',
-                interestRate: 0.001,
-                id: 3
-              }
-        ];
-        coinsModel.insertMany(defaultCoinsModel, function(err) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log('Valores predeterminados agregados');
-            }
-          });
-        }
-        });
+// coinsModel.countDocuments({}, function(err, count) {
+//     if (count === 0) {
+//         const defaultCoinsModel = [
+//             {
+//                 name: 'bitcoin',
+//                 interestRate: 0.005,
+//                 id: 1
+//               },
+//               {
+//                 name: 'etherium',
+//                 interestRate: 0.0042,
+//                 id: 2
+//               },
+//               {
+//                 name: 'cardano',
+//                 interestRate: 0.001,
+//                 id: 3
+//               }
+//         ];
+//         coinsModel.insertMany(defaultCoinsModel, function(err) {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               console.log('Valores predeterminados agregados');
+//             }
+//           });
+//         }
+//         });
 
 
 
